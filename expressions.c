7807caa7fbc46e84d1f5dbfa01a8e4537778d7ec
Mitali Sen <mitali.sen1307@gmail.com>
2021-05-11 07:55:55 +0100

    saved expressions

---
+++ expressions.c
@@ -0,0 +1,242 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "lexer.h"
+#include "parser.h"
+
+ParserInfo expressionList()
+{
+    printf("\n hello from expressionList");
+    ParserInfo pi;
+    pi.er = none;
+    pi.tk = PeekNextToken(); // change 1
+    printf("\nline 525 whats our token, its %s", pi.tk.lx);
+    if (strcmp(pi.tk.lx, "-") == 0 || strcmp(pi.tk.lx, "~") == 0 || pi.tk.tp == INT || pi.tk.tp == ID || pi.tk.tp == STRING || strcmp(pi.tk.lx, "true") == 0 || strcmp(pi.tk.lx, "false") == 0 || strcmp(pi.tk.lx, "null") == 0 || strcmp(pi.tk.lx, "this") == 0 || strcmp(pi.tk.lx, "(") == 0)
+    {
+        pi = expression();
+        if (pi.er != none)
+            return pi;
+        printf("\nline 532 whats our token, its %s", pi.tk.lx);
+        pi.tk = PeekNextToken();
+        printf("\nline 534 whats our token, its %s", pi.tk.lx);
+        while (1)
+        {
+            if (strcmp(pi.tk.lx, ",") != 0)
+                break;
+            printf("\nreally hope we're not here");
+            pi.tk = GetNextToken();
+            pi = expression();
+            if (pi.er != none)
+                return pi;
+            pi.tk = PeekNextToken();
+        }
+    }
+    else if (strcmp(pi.tk.lx, ")") == 0)
+        ;
+    else
+    {
+        pi.er = syntaxError;
+        return pi;
+    }
+    return pi;
+}
+
+ParserInfo expression()
+{
+    printf("\n hello from expression");
+    ParserInfo pi;
+    pi.er = none;
+    pi = relationalExpression();
+    if (pi.er != none)
+        return pi;
+    while (1)
+    {
+        if (strcmp(pi.tk.lx, "&") != 0 && strcmp(pi.tk.lx, "|") != 0)
+            break;
+        GetNextToken();
+        pi = relationalExpression();
+        if (pi.er != none)
+            return pi;
+    }
+    return pi;
+}
+
+ParserInfo relationalExpression()
+{
+    printf("\n hello from relationalExpression");
+    ParserInfo pi;
+    pi.er = none;
+    pi = ArithmeticExpression();
+    if (pi.er != none)
+        return pi;
+    while (1)
+    {
+        if (strcmp(pi.tk.lx, "=") != 0 && strcmp(pi.tk.lx, ">") != 0 && strcmp(pi.tk.lx, "<") != 0)
+            break;
+        printf(" line 592 arithmetic, token is %s", pi.tk.lx);
+        GetNextToken();
+        pi = ArithmeticExpression();
+        if (pi.er != none)
+            return pi;
+    }
+    return pi;
+}
+
+ParserInfo ArithmeticExpression()
+{
+    printf("\n hello from ArithmeticExpression");
+    ParserInfo pi;
+    pi.er = none;
+    pi = term();
+    if (pi.er != none)
+        return pi;
+    while (1)
+    {
+        if (strcmp(pi.tk.lx, "+") != 0 && strcmp(pi.tk.lx, "-") != 0)
+            break;
+        GetNextToken();
+        pi = term();
+        if (pi.er != none)
+            return pi;
+    }
+    return pi;
+}
+
+ParserInfo term()
+{
+    printf("\n hello from term");
+    ParserInfo pi;
+    pi.er = none;
+    pi = factor();
+    if (pi.er != none)
+        return pi;
+    while (1)
+    {
+        if (strcmp(pi.tk.lx, "*") != 0 && strcmp(pi.tk.lx, "/") != 0)
+            break;
+        GetNextToken();
+        pi = factor();
+        if (pi.er != none)
+            return pi;
+    }
+    return pi;
+}
+
+ParserInfo factor()
+{
+    printf("\n hello from factor");
+    ParserInfo pi;
+    pi.er = none;
+    pi = operand();
+    if (pi.er != none)
+        return pi;
+    while (1)
+    {
+        if (strcmp(pi.tk.lx, "*") != 0 && strcmp(pi.tk.lx, "/") != 0)
+            break;
+        GetNextToken();
+        pi = operand();
+        if (pi.er != none)
+            return pi;
+    }
+    return pi;
+}
+
+ParserInfo operand()
+{
+    printf("\n hello from operand");
+    ParserInfo pi;
+    pi.er = none;
+    pi.tk = GetNextToken();
+    printf("\nline 658 token: %s", pi.tk.lx);
+    if (strcmp(pi.tk.lx, "-") == 0 || strcmp(pi.tk.lx, "~") == 0 || pi.tk.tp == INT || pi.tk.tp == ID || pi.tk.tp == STRING || strcmp(pi.tk.lx, "true") == 0 || strcmp(pi.tk.lx, "false") == 0 || strcmp(pi.tk.lx, "null") == 0 || strcmp(pi.tk.lx, "this") == 0 || strcmp(pi.tk.lx, "(") == 0)
+        ;
+    else
+    {
+        pi.er = syntaxError;
+        return pi;
+    }
+    if (strcmp(pi.tk.lx, "(") == 0)
+    {
+        pi = expression();
+        if (pi.er != none)
+            return pi;
+        pi.tk = GetNextToken();
+        if (strcmp(pi.tk.lx, ")") == 0)
+            ;
+        else
+        {
+            pi.er = closeParenExpected;
+            return pi;
+        }
+    }
+    if (pi.tk.tp == ID)
+    {
+        printf("\ndid we at least get to ID?");
+        printf("\nline 688 token %s", pi.tk.lx);
+        while (1)
+        {
+            pi.tk = PeekNextToken();
+            if (strcmp(pi.tk.lx, ".") != 0)
+                break;
+            pi.tk = GetNextToken();
+            pi.tk = GetNextToken();
+            if (pi.tk.tp == ID)
+                ;
+            else
+            {
+                pi.er = idExpected;
+                return pi;
+            }
+            break;
+        }
+        printf("\nok are we at the choice of expression and expList?");
+        while (1)
+        {
+            pi.tk = PeekNextToken();
+            if (strcmp(pi.tk.lx, "[") != 0 && strcmp(pi.tk.lx, "(") != 0)
+                break;
+            printf("\noh did we find our ( ?");
+            pi.tk = GetNextToken();
+            if (strcmp(pi.tk.lx, "[") == 0)
+            {
+                pi = expression();
+                if (pi.er != none)
+                    return pi;
+                pi.tk = GetNextToken();
+                if (strcmp(pi.tk.lx, "]") == 0)
+                    ;
+                else
+                {
+                    pi.er = closeBraceExpected;
+                    return pi;
+                }
+                break;
+            }
+            else if (strcmp(pi.tk.lx, "(") == 0)
+            {
+                printf("\nshould be in expression list here");
+                printf("\nline 721 ok what token are we at? we are at : %s", pi.tk.lx);
+                printf("\nnow we call expression list");
+                pi = expressionList();
+                printf("\nok what token are we at? we are at : %s", pi.tk.lx);
+                printf("\nok line 723 error: %d", pi.er);
+                if (pi.er != none)
+                    return pi;
+                pi.tk = GetNextToken();
+                printf("\nline 727 ok what token are we at? we are at : %s", pi.tk.lx);
+                if (strcmp(pi.tk.lx, ")") == 0)
+                    ;
+                else
+                {
+                    pi.er = closeParenExpected;
+                    return pi;
+                }
+                break;
+            }
+        }
+    }
+    printf("\nok line 752 token: %s", pi.tk.lx);
+    return pi;
+}
--- parser.c
+++ parser.c
@@ -99,18 +99,20 @@ ParserInfo subroutineDeclar()
 		pi.er = subroutineDeclarErr;
 		return pi;
 	}
+	printf("\ntoken in subDeclar line 102 %s", pi.tk.lx);
 	pi.tk = PeekNextToken();
+	printf("\ntoken in subDeclar line 104 %s", pi.tk.lx);
 	if (strcmp(pi.tk.lx, "char") == 0 || strcmp(pi.tk.lx, "int") == 0 || strcmp(pi.tk.lx, "boolean") == 0 || pi.tk.tp == ID)
-		type();
+		pi = type();
 	else if (strcmp(pi.tk.lx, "void") == 0)
-		;
+		GetNextToken();
 	else
 	{
 		pi.er = syntaxError;
 		return pi;
 	}
 	pi.tk = GetNextToken();
-	pi.tk = GetNextToken();
+	printf("\ntoken in subDeclar line 115 %s", pi.tk.lx);
 	if (pi.tk.tp == ID)
 		;
 	else
@@ -314,7 +316,7 @@ ParserInfo letStatement()
 	{
 		if (strcmp(pi.tk.lx, "[") != 0)
 			break;
-		printf("hi we shouldnt be here");
+		printf("\nline 317 letStatement token %s", pi.tk.lx);
 		pi = expression();
 		if (pi.er != none)
 			return pi;
@@ -368,6 +370,7 @@ ParserInfo ifStatement()
 	pi = expression();
 	if (pi.er != none)
 		return pi;
+	pi.tk = GetNextToken();
 	if (strcmp(pi.tk.lx, ")") == 0)
 		;
 	else
@@ -408,6 +411,7 @@ ParserInfo whileStatement()
 	pi = expression();
 	if (pi.er != none)
 		return pi;
+	pi.tk = GetNextToken();
 	if (strcmp(pi.tk.lx, ")") == 0)
 		;
 	else
@@ -503,9 +507,12 @@ ParserInfo subroutineCall()
 		pi.er = openParenExpected;
 		return pi;
 	}
+	printf("\nline 506 token %s", pi.tk.lx);
 	pi = expressionList();
 	if (pi.er != none)
 		return pi;
+	printf("\nline 506 token %s", pi.tk.lx);
+	pi.tk = GetNextToken();
 	if (strcmp(pi.tk.lx, ")") == 0)
 		;
 	else
@@ -516,240 +523,7 @@ ParserInfo subroutineCall()
 	return pi;
 }
 
-ParserInfo expressionList()
-{
-	printf("\n hello from expressionList");
-	ParserInfo pi;
-	pi.er = none;
-	pi.tk = PeekNextToken();
-	printf("\nline 525 whats our token, its %s", pi.tk.lx);
-	if (strcmp(pi.tk.lx, "-") == 0 || strcmp(pi.tk.lx, "~") == 0 || pi.tk.tp == INT || pi.tk.tp == ID || pi.tk.tp == STRING || strcmp(pi.tk.lx, "true") == 0 || strcmp(pi.tk.lx, "false") == 0 || strcmp(pi.tk.lx, "null") == 0 || strcmp(pi.tk.lx, "this") == 0 || strcmp(pi.tk.lx, "(") == 0)
-	{
-		pi = expression();
-		if (pi.er != none)
-			return pi;
-		//pi.tk = GetNextToken();
-		printf("\nline 532 whats our token, its %s", pi.tk.lx);
-		pi.tk = PeekNextToken();
-		printf("\nline 534 whats our token, its %s", pi.tk.lx);
-		while (1)
-		{
-			if (strcmp(pi.tk.lx, ",") != 0)
-				break;
-			printf("\nreally hope we're not here");
-			pi.tk = GetNextToken();
-			pi = expression();
-			if (pi.er != none)
-				return pi;
-			pi.tk = PeekNextToken();
-		}
-	}
-	else if (strcmp(pi.tk.lx, ")") == 0)
-		;
-	else
-	{
-		pi.er = syntaxError;
-		return pi;
-	}
-	return pi;
-}
-
-ParserInfo expression()
-{
-	printf("\n hello from expression");
-	ParserInfo pi;
-	pi.er = none;
-	pi = relationalExpression();
-	if (pi.er != none)
-		return pi;
-	while (1)
-	{
-		if (strcmp(pi.tk.lx, "&") != 0 && strcmp(pi.tk.lx, "|") != 0)
-			break;
-		pi = relationalExpression();
-		if (pi.er != none)
-			return pi;
-	}
-	return pi;
-}
-
-ParserInfo relationalExpression()
-{
-	printf("\n hello from relationalExpression");
-	ParserInfo pi;
-	pi.er = none;
-	pi = ArithmeticExpression();
-	if (pi.er != none)
-		return pi;
-	while (1)
-	{
-		if (strcmp(pi.tk.lx, "=") != 0 && strcmp(pi.tk.lx, ">") != 0 && strcmp(pi.tk.lx, "<") != 0)
-			break;
-		pi = ArithmeticExpression();
-		if (pi.er != none)
-			return pi;
-	}
-	return pi;
-}
-
-ParserInfo ArithmeticExpression()
-{
-	printf("\n hello from ArithmeticExpression");
-	ParserInfo pi;
-	pi.er = none;
-	pi = term();
-	if (pi.er != none)
-		return pi;
-	while (1)
-	{
-		if (strcmp(pi.tk.lx, "+") != 0 && strcmp(pi.tk.lx, "-") != 0)
-			break;
-		pi = term();
-		if (pi.er != none)
-			return pi;
-	}
-	return pi;
-}
-
-ParserInfo term()
-{
-	printf("\n hello from term");
-	ParserInfo pi;
-	pi.er = none;
-	pi = factor();
-	if (pi.er != none)
-		return pi;
-	while (1)
-	{
-		if (strcmp(pi.tk.lx, "*") != 0 && strcmp(pi.tk.lx, "/") != 0)
-			break;
-		pi = factor();
-		if (pi.er != none)
-			return pi;
-	}
-	return pi;
-}
-
-ParserInfo factor()
-{
-	printf("\n hello from factor");
-	ParserInfo pi;
-	pi.er = none;
-	pi = operand();
-	if (pi.er != none)
-		return pi;
-	while (1)
-	{
-		if (strcmp(pi.tk.lx, "*") != 0 && strcmp(pi.tk.lx, "/") != 0)
-			break;
-		pi = operand();
-		if (pi.er != none)
-			return pi;
-	}
-	return pi;
-}
-
-ParserInfo operand()
-{
-	printf("\n hello from operand");
-	ParserInfo pi;
-	pi.er = none;
-	pi.tk = GetNextToken();
-	if (strcmp(pi.tk.lx, "-") == 0 || strcmp(pi.tk.lx, "~") == 0 || pi.tk.tp == INT || pi.tk.tp == ID || pi.tk.tp == STRING || strcmp(pi.tk.lx, "true") == 0 || strcmp(pi.tk.lx, "false") == 0 || strcmp(pi.tk.lx, "null") == 0 || strcmp(pi.tk.lx, "this") == 0 || strcmp(pi.tk.lx, "(") == 0)
-		;
-	else
-	{
-		pi.er = syntaxError;
-		return pi;
-	}
-	if (strcmp(pi.tk.lx, "(") == 0)
-	{
-		pi = expression();
-		if (pi.er != none)
-			return pi;
-		pi.tk = PeekNextToken();
-		if (strcmp(pi.tk.lx, ")") == 0)
-			;
-		else
-		{
-			pi.er = closeParenExpected;
-			return pi;
-		}
-	}
-	if (pi.tk.tp == ID)
-	{
-		printf("\ndid we at least get to ID?");
-		printf("\nline 682 token %s", pi.tk.lx);
-		pi.tk = PeekNextToken();
-		printf("\nline 684 token %s", pi.tk.lx);
-		while (1)
-		{
-			if (strcmp(pi.tk.lx, ".") != 0)
-				break;
-			pi.tk = GetNextToken();
-			pi.tk = GetNextToken();
-			if (pi.tk.tp == ID)
-				;
-			else
-			{
-				pi.er = idExpected;
-				return pi;
-			}
-			break;
-		}
-		printf("\nok are we at the choice of expression and expList?");
-		pi.tk = PeekNextToken();
-		while (1)
-		{
-			if (strcmp(pi.tk.lx, "[") != 0 && strcmp(pi.tk.lx, "(") != 0)
-				break;
-			printf("\noh did we find our ( ?");
-			pi.tk = GetNextToken();
-			if (strcmp(pi.tk.lx, "[") == 0)
-			{
-				pi = expression();
-				if (pi.er != none)
-					return pi;
-				pi.tk = GetNextToken();
-				if (strcmp(pi.tk.lx, "]") == 0)
-					;
-				else
-				{
-					pi.er = closeBraceExpected;
-					return pi;
-				}
-				break;
-			}
-			else if (strcmp(pi.tk.lx, "(") == 0)
-			{
-				printf("\nshould be in expression list here");
-				printf("\nline 721 ok what token are we at? we are at : %s", pi.tk.lx);
-				printf("\nnow we call expression list");
-				pi = expressionList();
-				printf("\nok what token are we at? we are at : %s", pi.tk.lx);
-				printf("\nok line 723 error: %d", pi.er);
-				if (pi.er != none)
-					return pi;
-				pi.tk = GetNextToken();
-				printf("\nline 727 ok what token are we at? we are at : %s", pi.tk.lx);
-				if (strcmp(pi.tk.lx, ")") == 0)
-					;
-				else
-				{
-					pi.er = closeParenExpected;
-					return pi;
-				}
-				break;
-			}
-			else
-			{
-				pi.er = syntaxError;
-				return pi;
-			}
-		}
-	}
-	return pi;
-}
+// put expressions back here
 
 ParserInfo memberDeclar()
 {
@@ -839,10 +613,11 @@ int StopParser()
 #ifndef TEST_PARSER
 int main()
 {
-	InitParser("Square.jack");
+	InitParser("Ball.jack");
 	ParserInfo pi;
 	pi = Parse();
-	printf("\n token lexeme: %s and pi error: %d", pi.tk.lx, pi.er);
+	StopParser();
+	printf("\n token lexeme: %s, line no: %d and pi error: ", pi.tk.lx, pi.tk.ln);
 	if (pi.er == 0)
 		printf("none");
 	if (pi.er == 1)
@@ -875,7 +650,7 @@ int main()
 		printf("equalExpected");
 	if (pi.er == 15)
 		printf("syntaxError");
-	printf("\n");
+	printf("\n\n\n");
 	return 1;
 }
 #endif
